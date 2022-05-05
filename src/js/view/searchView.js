6a8afc83cdb00fcb3609a01ac1e549bcb68bb7f9
ggcode404 <101041726+ggcode404@users.noreply.github.com>
2022-05-05 18:51:26 +0400

    title reduce

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -8,6 +8,26 @@ export const clearResults = () =>{
    elements.searchResPages.innerHTML = '';
 } 
 
+const convertTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <= limit){
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+
+        }, 0);
+
+        return `${newTitle.join(' ')}...`;
+    }
+
+
+
+    return title;
+}
+
+
 
 
 const renderRecipe = (recipe) => {
@@ -17,7 +37,7 @@ const renderRecipe = (recipe) => {
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${convertTitle(recipe.title)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
