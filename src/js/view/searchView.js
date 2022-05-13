26231e8f70204154e14a73df613b4ab7e4c80916
ggcode404 <101041726+ggcode404@users.noreply.github.com>
2022-05-13 14:12:02 +0400

    #  add  HREF

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -32,7 +32,7 @@ const convertTitle = (title, limit = 17) => {
 
 const renderRecipe = (recipe) => {
     const markup = `<li>
-    <a class="results__link " href="${recipe.recipe_id}">
+    <a class="results__link " href="#${recipe.recipe_id}">
         <figure class="results__fig">
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
