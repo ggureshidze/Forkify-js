a14fc9a53e2e9eeb51a7b0001eda5e7ee88e531b
ggcode404 <101041726+ggcode404@users.noreply.github.com>
2022-05-13 14:08:05 +0400

    clas recipe id

---
+++ src/js/models/recipe.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+
+export default class Recipe {
+    constructor(id){
+        this.id = id
+
+    }
+
+    async getRecipe(){
+
+
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+            this.title = res.data.recipe.title;
+            this.author = res.data.recipe.publisher;
+            this.img = res.data.recipe.image_url;
+            this.url = res.data.recipe.source_url;
+            this.ingredients = res.data.recipe.ingredients;
+        } catch (error) {
+            alert(error)
+            
+        }
+        
+
+
+
+
+
+
+    }
+
+}
